10d272d3044baa286b802df5080fa4da3e210856
Mohammed H. AlMuhanna <almuhannamh@outlook.com>
2020-04-07 01:21:13 +0300

    add communities

---
+++ db/migrate/20200406221719_create_communities.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class CreateCommunities < ActiveRecord::Migration[5.2]
+  def change
+    create_table :communities do |t|
+      t.string :name
+      t.string :url
+      t.text :rules
+      t.integer :total_members
+
+      t.timestamps
+    end
+  end
+end
