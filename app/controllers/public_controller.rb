71ad27923ee2a1b6fc194b051926a94792bf5709
Mohammed H. AlMuhanna <almuhannamh@outlook.com>
2020-04-07 10:02:38 +0300

    add Settigns for edit user's imfo

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -4,5 +4,9 @@ class PublicController < ApplicationController
   def index
     @communities = Community.all.limit(3)
     @posts = Post.order(id: :desc).limit(2)
-end
+  end
+
+  # def full_name
+  #   "#{current_account.first_name} #{current_account.last_name}"
+  # end
 end
