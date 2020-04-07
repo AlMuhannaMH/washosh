450963a6dae21baae8a5ccdd0c2d6a2ffc69a5bb
Mohammed H. AlMuhanna <almuhannamh@outlook.com>
2020-04-07 03:38:36 +0300

    add create controller and refactor some code.

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -2,6 +2,7 @@
 
 class CommunitiesController < ApplicationController
   before_action :authenticate_account!, except: %i[index show]
+  before_action :set_community, only: [:show]
 
   def index
     @communities = Community.all
@@ -13,5 +14,24 @@ class CommunitiesController < ApplicationController
     @community = Community.new
   end
 
-  def create; end
+  def create
+    @community = Community.new(community_params)
+    @community.account = current_account
+
+    if @community.save
+      redirect_to @community
+    else
+      render 'new'
+    end
+    end
+
+  private
+
+  def community_params
+    params.require(:community).permit(:name, :url, :rules)
+  end
+
+  def set_community
+    @community = Community.find(params[:id])
+  end
 end
