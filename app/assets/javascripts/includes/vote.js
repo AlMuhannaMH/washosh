f34e2b6989779db90d848f57aa5ec8f66dbd3cd9
Mohammed H. AlMuhanna <almuhannamh@outlook.com>
2020-04-08 02:49:03 +0300

    add the logic to handle the votes

--- app/assets/javascripts/includes/vote.js
+++ app/assets/javascripts/includes/vote.js
@@ -1,5 +1,12 @@
-$(function(){
-  $(".vote").on("click", ".upvote, .downvote", function(){
-  	console.log("I amd from vote.js, can you hear me?")
+$(() => {
+  $(".vote").on("click", ".upvote, .downvote", () => {
+    let post_id = $(this).parent().data("id"), is_upvote = $(this).hasClass("upvote");
+
+    $.ajax({
+      url: "/post/vote",
+      method: "POST",
+      data: { post_id: post_id, upvote: is_upvote },
+      success: () => {console.log("success..");}
+    });
   });
 });
