45966b8831eb7799e03e944f5938e44bb8b8b61b
Mohammed H. AlMuhanna <almuhannamh@outlook.com>
2020-04-07 21:26:04 +0300

    migration create_comments

---
+++ db/migrate/20200407182326_create_comments.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class CreateComments < ActiveRecord::Migration[5.2]
+  def change
+    create_table :comments do |t|
+      t.references :account
+      t.references :post
+      t.text :message
+      t.timestamps
+    end
+  end
+end
